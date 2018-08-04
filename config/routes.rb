b6b490a13e28bf1afd4a3a51afd4f3efa876b411
Liam Hockley <xdega121@gmail.com>
2018-08-04 15:59:58 -0500

    Update mailer config for Devise; Update paths.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { 
+      sign_in: 'login', 
+      sign_out: 'logout', 
+      sign_up: 'register'
+  }
   root 'welcome#index'
 end
